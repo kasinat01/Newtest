29aba0ecc07d7f9b61357ac22679dff4d940e656
Kasinat <yagamina@gmail.com>
2021-02-19 17:03:29 +0700

    master

--- school/views.py
+++ school/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from school.models import School, Student
+from school.serializer import SchoolSerializers, StudentSerializers
+
+
+class SchoolList(generics.ListCreateAPIView):
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializers
+
+
+class SchoolDetails(generics.RetrieveUpdateDestroyAPIView):
+    def get_queryset(self):
+        return School.objects.filter(id=self.kwargs.get('pk', None))
+
+    serializer_class = SchoolSerializers
+
+
+class StudentList(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+
+
+class StudentDetails(generics.RetrieveUpdateDestroyAPIView):
+    def get_queryset(self):
+        return Student.objects.filter(id=self.kwargs.get('pk', None))
+
+    serializer_class = StudentSerializers
